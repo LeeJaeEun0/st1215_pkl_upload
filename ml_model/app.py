2760762a668f7106a09a35d94af45145aef57486
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-15 17:14:40 +0900

    update_checkbox

--- ml_model/app.py
+++ ml_model/app.py
@@ -2,4 +2,8 @@ import streamlit as st
 import pandas as pd
 
 df=pd.read_csv("https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv")
-st.write(df)
+
+if st.checkbox('원본 데이터 보기'):
+    st.subheader('Raw data')
+    st.write(df)
+
