200bc691628171eb96a332dd1d6cb592e0d43292
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-15 17:15:28 +0900

    update_title

--- ml_model/app.py
+++ ml_model/app.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 df=pd.read_csv("https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv")
 
+st.title('데이터 모델 배포하기')
+
 if st.checkbox('원본 데이터 보기'):
     st.subheader('Raw data')
     st.write(df)
