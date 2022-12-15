370aabec7ae7db5afa2b686199b070efee1a6439
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-15 17:20:37 +0900

    update_os

--- ml_model/app.py
+++ ml_model/app.py
@@ -9,3 +9,10 @@ if st.checkbox('원본 데이터 보기'):
     st.subheader('Raw data')
     st.write(df)
 
+import joblib
+import os
+# os.path... 파이썬 경로문제 해결
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
+model = joblib.load(model_path)
+st.write("## 선형 회귀 모델")
+st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
